ae39c2cbeae56d6662b7fa3e7184d57a204423a9
Nick Foden <nickfoden@gmail.com>
2021-08-02 16:24:08 -0700

    add another smoke test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,3 +8,11 @@ describe("Test Root", () => {
     expect(response.body.data).toBe("Worldly Hellos");
   });
 });
+
+describe("Smoke Test", () => {
+  test("GET root returns 200 and text", async () => {
+    const response = await request(app).get("/");
+    expect(response.statusCode).toBe(200);
+    expect(response.body.data).toBe("Worldly Hellos");
+  });
+});
